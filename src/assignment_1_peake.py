d4fa6ab9914491d6ce121f84428ce50b03750834
peake-s <peakes@vcu.edu>
2022-09-13 11:08:16 -0400

    fixing normalization errors

--- src/assignment_1_peake.py
+++ src/assignment_1_peake.py
@@ -7,11 +7,19 @@ df_a =  pd.read_csv("../data/groupA.txt", header=None, names = ["cost", "weight"
 df_b =  pd.read_csv("../data/groupB.txt", header=None, names = ["cost", "weight", "type"])
 df_c =  pd.read_csv("../data/groupC.txt", header=None, names = ["cost", "weight", "type"])
 
-normalized_df_a = (df_a-df_a.min())/(df_a.max()-df_a.min())
-normalized_df_b = (df_b-df_b.min())/(df_b.max()-df_b.min())
-normalized_df_c = (df_a-df_c.min())/(df_c.max()-df_c.min())
+normalized_df_a = df_a.copy()
+normalized_df_b = df_b.copy()
+normalized_df_c = df_c.copy()
 
-#price on x, weight on y
+normalized_df_a['cost'] = (df_a['cost']-df_a['cost'].min())/(df_a['cost'].max()-df_a['cost'].min())
+normalized_df_a['weight'] = (df_a['weight']-df_a['weight'].min())/(df_a['weight'].max()-df_a['weight'].min())
+
+normalized_df_b['cost'] = (df_b['cost']-df_b['cost'].min())/(df_b['cost'].max()-df_b['cost'].min())
+normalized_df_b['weight'] = (df_b['weight']-df_b['weight'].min())/(df_b['weight'].max()-df_b['weight'].min())
+
+normalized_df_c['cost'] = (df_c['cost']-df_c['cost'].min())/(df_c['cost'].max()-df_c['cost'].min())
+normalized_df_c['weight'] = (df_c['weight']-df_c['weight'].min())/(df_c['weight'].max()-df_c['weight'].min())
+
 plt.figure()
 plt.scatter(normalized_df_a['cost'],normalized_df_a['weight'],c=df_a['type'])
 plt.xlabel('cost (USD)')
